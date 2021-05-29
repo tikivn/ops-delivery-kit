f626898fd52d9725e3b5e886d0729494e531c8bd
dat.dao <dat.dao@tiki.vn>
2021-05-29 21:00:32 +0700

    [Lib] Update luffy client

--- luffy/client.go
+++ luffy/client.go
@@ -87,31 +87,31 @@ type shipcodeResponse struct {
 // 	return RequestCreateResponse{}, errors.Errorf("luffy: server response status code = %d, payload = %+v, response = %s", res.StatusCode, string(body), result)
 // }
 
-func (c *client) GetQuotes(ctx context.Context, payload Request) ([]QuotesResult, error) {
+func (c *client) GetQuotes(ctx context.Context, payload Request) (QuotesResult, error) {
 	path := fmt.Sprintf("%v/v1/request/create", c.host)
 
 	body, err := json.Marshal(payload)
 	if err != nil {
-		return []QuotesResult{}, errors.Wrapf(err, "luffy: Cant marshal request body")
+		return QuotesResult{}, errors.Wrapf(err, "luffy: Cant marshal request body")
 	}
 
 	req, err := http.NewRequest("POST", path, bytes.NewBuffer(body))
 	if err != nil {
-		return []QuotesResult{}, errors.Wrapf(err, "luffy: Cant create Request")
+		return QuotesResult{}, errors.Wrapf(err, "luffy: Cant create Request")
 	}
 	req.Header.Add("Content-Type", "application/json")
 	req = req.WithContext(ctx)
 
 	res, err := c.httpDoer.Do(req)
 	if err != nil {
-		return []QuotesResult{}, errors.Wrapf(err, "luffy: Response error for query")
+		return QuotesResult{}, errors.Wrapf(err, "luffy: Response error for query")
 	}
 	defer res.Body.Close()
 
 	result, err := ioutil.ReadAll(res.Body)
 
 	if res.StatusCode == http.StatusOK {
-		e := []QuotesResult{}
+		e := QuotesResult{}
 
 		if err := json.Unmarshal(result, &e); err != nil {
 			return e, errors.Wrapf(err, "luffy: couldnt decode json, body %s", string(body))
@@ -120,5 +120,5 @@ func (c *client) GetQuotes(ctx context.Context, payload Request) ([]QuotesResult
 		return e, nil
 	}
 
-	return []QuotesResult{}, errors.Errorf("luffy: server response status code = %d, payload = %+v, response = %s", res.StatusCode, string(body), result)
+	return QuotesResult{}, errors.Errorf("luffy: server response status code = %d, payload = %+v, response = %s", res.StatusCode, string(body), result)
 }
