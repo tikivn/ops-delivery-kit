cdc1a475a4bc64b251fba6c55cf568ae6ba01229
Chinh Nguyen <chinh.nguyen1@tiki.vn>
2021-09-08 17:53:06 +0700

    Update TMS Client

--- tms/payload.go
+++ tms/payload.go
@@ -1,12 +1,13 @@
 package tms
 
 type Payload struct {
-	BoxCode         string       `json:"box_code"`
-	BoxRouteID      string       `json:"box_route_id"`
-	PartnerCode     string       `json:"partner_code"`
-	BusinessType    BusinessType `json:"business_type"`
-	ServiceCode     string       `json:"service_code"`
-	RefType         string       `json:"ref_type"`
-	OldRouteID      string       `json:"old_route_id"`
-	StandardBoxCode string       `json:"standard_box_code"`
+	BoxCode            string       `json:"box_code"`
+	BoxRouteID         string       `json:"box_route_id"`
+	PartnerCode        string       `json:"partner_code"`
+	BusinessType       BusinessType `json:"business_type"`
+	ServiceCode        string       `json:"service_code"`
+	RefType            string       `json:"ref_type"`
+	OldRouteID         string       `json:"old_route_id"`
+	StandardBoxCode    string       `json:"standard_box_code"`
+	LatestTrackingCode string       `json:"latest_tracking_code"`
 }
