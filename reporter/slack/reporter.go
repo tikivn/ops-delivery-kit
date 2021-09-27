8726a9ea344096fc47fb17825014498e2a883c54
Chinh Nguyen <chinh.nguyen1@tiki.vn>
2021-09-27 11:13:22 +0700

    Slack Reporter

---
+++ reporter/slack/reporter.go
@@ -0,0 +1,71 @@
+package slack
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+)
+
+type Reporter interface {
+	Alert(message string)
+}
+
+type reporter struct {
+	httpDoer      *http.Client
+	webhookURL    string
+	mentionGroups []string
+	mentionUsers  []string
+}
+
+type reporterRequest struct {
+	Text string `json:"text"`
+}
+
+func (r reporter) Alert(message string) {
+	go func() {
+		mentions := ""
+		for _, group := range r.mentionGroups {
+			mentions += fmt.Sprintf("<!%s> ", group)
+		}
+		for _, user := range r.mentionUsers {
+			mentions += fmt.Sprintf("<@%s> ", user)
+		}
+		if mentions != "" {
+			mentions += "\n"
+		}
+
+		reporterRequest := reporterRequest{
+			Text: fmt.Sprintf("%s%s", mentions, message),
+		}
+
+		body, err := json.Marshal(reporterRequest)
+		if err != nil {
+			logrus.Error(err)
+			return
+		}
+
+		request, err := http.NewRequest("POST", r.webhookURL, bytes.NewBuffer(body))
+		if err != nil {
+			logrus.Error(err)
+			return
+		}
+		request.Header.Add("Content-Type", "application/json")
+
+		_, err = r.httpDoer.Do(request)
+		if err != nil {
+			logrus.Error(err)
+		}
+	}()
+}
+
+func NewReporter(webhookURL string, mentionGroups []string, mentionUsers []string) Reporter {
+	return reporter{
+		httpDoer:      http.DefaultClient,
+		webhookURL:    webhookURL,
+		mentionGroups: mentionGroups,
+		mentionUsers:  mentionUsers,
+	}
+}
