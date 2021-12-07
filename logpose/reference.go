98296aa235fb15af1389cf6abedfff2d54ba4708
chungtm-tiki <chung.tran1@tiki.vn>
2021-12-07 13:39:37 +0700

    update ActiveDriversResult model

--- logpose/reference.go
+++ logpose/reference.go
@@ -2,6 +2,7 @@ package logpose
 
 type ActiveDriversResult struct {
 	DriverID    string `json:"driver_id"'`
+	ThemisID    string `json:"themis_id"`
 	DriverName  string `json:"driver_name"'`
 	Coordinates struct {
 		Lat float64 `json:"lat"`
