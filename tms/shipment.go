857ac2fcdef17043887b397818a5a6b07e9b779e
Chinh Nguyen <chinh.nguyen1@tiki.vn>
2022-04-25 13:26:31 +0700

    TMS: shipment tracking payload

--- tms/shipment.go
+++ tms/shipment.go
@@ -26,6 +26,7 @@ func (t StTimestamp) MarshalJSON() ([]byte, error) {
 
 type ShipmentTracking struct {
 	RefCode   string      `json:"ref_code"`
+	BoxCode   string      `json:"box_code"`
 	Action    stAction    `json:"action"`
 	Timestamp StTimestamp `json:"timestamp"`
 	PartnerID string      `json:"partner_id"`
